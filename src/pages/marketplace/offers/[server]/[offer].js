a7605ebcb1ecc98cc23d15699bc564ee5052e72a
tsquix <tsquix@gmail.com>
2025-05-09 14:08:33 +0200

    fixes for deployment

--- src/pages/marketplace/offers/[server]/[offer].js
+++ src/pages/marketplace/offers/[server]/[offer].js
@@ -13,16 +13,16 @@ export default function OfferDetailPage({ selectedOffer, handleBuy }) {
     <>
       <div className="bg-mainBg p-6 flex flex-col rounded-3xl w-full ">
         <p className="mb-4">
-          <strong>{selectedOffer.title || 'title'}</strong>
+          <strong>{selectedOffer?.title || 'title'}</strong>
         </p>
         <div className="mb-4 text-xs flex flex-col">
           <div className="mb-2 flex items-center gap-2">
             <span> Sprzedawca:</span>
             <span className="bg-brighterBg px-3 py-2  rounded-2xl">
-              <Link href={`/profile/${selectedOffer.seller.name}`}>
+              <Link href={`/profile/${selectedOffer?.seller.name}`}>
                 <strong className="text-xs text-red-300 ">
                   {' '}
-                  {selectedOffer.seller.name}
+                  {selectedOffer?.seller.name}
                 </strong>
               </Link>
             </span>
@@ -33,7 +33,7 @@ export default function OfferDetailPage({ selectedOffer, handleBuy }) {
               <span className="bg-brighterBg px-3 py-2 rounded-2xl">
                 <strong className="text-xs text-red-300 ">
                   {' '}
-                  {selectedOffer.seller.transactionCount}
+                  {selectedOffer?.seller.transactionCount}
                 </strong>
               </span>
             </div>
@@ -60,7 +60,7 @@ export default function OfferDetailPage({ selectedOffer, handleBuy }) {
           <div>
             {' '}
             <p className="text-sm font-bold mb-2">
-              {selectedOffer.currencyAmount} kk - {selectedOffer.pricePLN} PLN
+              {selectedOffer?.currencyAmount} kk - {selectedOffer?.pricePLN} PLN
             </p>{' '}
           </div>
           <div>
@@ -68,7 +68,7 @@ export default function OfferDetailPage({ selectedOffer, handleBuy }) {
             <p>
               Platnosc:{' '}
               <span className="text-xs text-red-300">
-                {selectedOffer.seller.prefPayment}
+                {selectedOffer?.seller.prefPayment}
               </span>
             </p>
           </div>
