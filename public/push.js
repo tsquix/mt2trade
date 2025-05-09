4df06a759a07253dfe5347eea3d89e379324a35d
tsquix <tsquix@gmail.com>
2025-05-09 14:37:50 +0200

    sw fix v2

--- public/push.js
+++ public/push.js
@@ -12,5 +12,14 @@ push.setVapidDetails(
   vapidKeys.privateKey
 );
 
-let sub = {};
-push.sendNotification(subscribe, 'testmessage');
+let sub = {
+  endpoint:
+    'https://fcm.googleapis.com/fcm/send/d5CD8lmYstg:APA91bFRhHcLMWZFyOjeKwjfuFqnu7SlcPXEKlEPlkdC1jG_AByg1V1kdxGvABzhJ_u9wMvLAUiE36xCPiIs_vfB_AKkD6qeV9oo_bPC6_oJWicYWhdRfkSlDgib-WkhJ_T3vXiYhpjf',
+  expirationTime: null,
+  keys: {
+    p256dh:
+      'BNn_AYsrUIYS_gQ2gjd7lVpA-uMV6KwKRyYVDdDSR8a54vAVJkl_-F4mOEOLzorWfTJCBV7E2QPD_RCVtdqHQus',
+    auth: '7KB2b5MnpGNqcnXfoXUKzw',
+  },
+};
+push.sendNotification(sub, 'testmessage');
--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('push', () => {
-  self.registration.sendNotification('test message', {});
+self.addEventListener('push', function (e) {
+  var options = {
+    body: 'This notification was generated from a push!',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2',
+    },
+    actions: [
+      {
+        action: 'explore',
+        title: 'Explore this new world',
+      },
+      { action: 'close', title: 'Close' },
+    ],
+  };
+  e.waitUntil(self.registration.showNotification('Hello world!', options));
 });
