e5bad176dcead7883892298859c52ecf1f7c8f8e
tsquix <tsquix@gmail.com>
2025-05-20 15:53:59 +0200

    orderpage refactor / add usereducer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,18 @@
 import '@/styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
+import { socket } from '../../public/socket';
+import { useEffect } from 'react';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  useEffect(() => {
+    socket.connect(); // tylko raz
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
--- src/pages/orders/index.js
+++ src/pages/orders/index.js
@@ -1,51 +1,72 @@
 import PushNotification from '@/components/notification/PushNotification';
-import OfferCard from '@/components/OfferCard';
 import Layout from '@/pages/layout';
 import axios from 'axios';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { useCallback, useEffect, useState } from 'react';
-import { io } from 'socket.io-client';
+import { useEffect, useReducer } from 'react';
+import { socket } from '../../../public/socket.js';
+
 import Swal from 'sweetalert2';
 export default function OrdersPage() {
-  const [view, setView] = useState('buy');
-  const [orders, setOrders] = useState({ buyOrders: [], sellOrders: [] });
+  const { data: session } = useSession();
+  const initialState = {
+    view: 'buy',
+    orders: {
+      buyOrders: [],
+      sellOrders: [],
+    },
+    pendingOffersId: [],
+    isLoading: false,
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { view, orders, pendingOffersId, isLoading } = state;
   const { buyOrders, sellOrders } = orders;
 
-  const { data: session } = useSession();
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'SET_VIEW':
+        return { ...state, view: action.payload };
+      case 'SET_ORDERS':
+        return { ...state, orders: action.payload };
 
-  const [selectedOffer, setSelectedOffer] = useState(null);
-  const [pendingOffersId, setPendingOrdersId] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const socket = io('http://localhost:4001');
+      case 'UPDATE_ORDER_STATUS':
+        const { orderId: id, status } = action.payload;
+        const orderToUpdate = state.orders.buyOrders.find(
+          (order) => order._id === id
+        );
+        if (!orderToUpdate) return state;
+        const updatedOrder = { ...orderToUpdate, orderStatus: status };
+        const updatedBuyOrders = state.orders.buyOrders.map((order) =>
+          order._id === id ? updatedOrder : order
+        );
+        return {
+          ...state,
+          orders: {
+            ...state.orders,
+            buyOrders: updatedBuyOrders,
+          },
+        };
+      case 'SET_PENDING_OFFERS':
+        return { ...state, pendingOffersId: action.payload };
+      case 'SET_LOADING':
+        return { ...state, isLoading: action.payload };
+      default:
+        return state;
+    }
+  }
 
   useEffect(() => {
     // dołącz do pokoju zamówienia
+    if (!pendingOffersId || pendingOffersId.length === 0) return;
     socket.emit('join-order-room', pendingOffersId);
 
     // nasłu chuj aktualizacji statusu
-    socket.on('order-updated', ({ orderId: id, status }) => {
-      if (pendingOffersId.includes(id)) {
-        // console.log('success');
-        // console.log(buyOrders.filter((order) => order._id === id).orderStatus);
-
-        const orderToUpdate = buyOrders.find((order) => order._id === id);
-        if (!orderToUpdate) return;
-        const updatedOrder = { ...orderToUpdate, orderStatus: status };
-        // console.log(updatedOrder);
-
-        // const newBuyOrdersArray = buyOrders.filter((order) => (order.id == id));
-        setOrders((prev) => ({
-          ...prev,
-          buyOrders: [
-            ...prev.buyOrders.filter((order) => order._id !== id),
-            updatedOrder,
-          ],
-        }));
-        // console.log(orders);
+    socket.on('order-updated', ({ orderId, status }) => {
+      if (pendingOffersId.includes(orderId)) {
+        dispatch({ type: 'UPDATE_ORDER_STATUS', payload: { orderId, status } });
       }
     });
-
+    //funkcja czysczaca
     return () => {
       socket.off('order-updated');
     };
@@ -58,10 +79,8 @@ export default function OrdersPage() {
     });
 
     try {
-      setIsLoading(true);
-      //BUG pass status  n order as object
+      dispatch({ type: 'SET_LOADING', payload: true });
       await axios.put('/api/buyOrder', { status, orderId });
-      // await axios.put('/api/buyOrder', status, orderId);
     } catch (err) {
       console.error(err);
     } finally {
@@ -69,8 +88,7 @@ export default function OrdersPage() {
         'buyorder status updated successfully : ' + status + ' ' + orderId
       );
       await fetchBuyOrders();
-
-      setIsLoading(false);
+      dispatch({ type: 'SET_LOADING', payload: false });
     }
   }
 
@@ -80,17 +98,18 @@ export default function OrdersPage() {
       const response = await axios.get(
         `/api/buyOrder?userId=${session?.user.id}`
       );
-      setOrders(response.data.orders);
+      dispatch({ type: 'SET_ORDERS', payload: response.data.orders });
 
-      setPendingOrdersId(
-        response.data.orders?.buyOrders
-          ?.filter((order) => order.orderStatus === 'pending')
-          .map((order) => order._id)
-      );
+      const pendingOffers = response.data.orders?.buyOrders
+        ?.filter((order) => order.orderStatus === 'pending')
+        .map((order) => order._id);
+
+      if (pendingOffers.length > 0)
+        dispatch({ type: 'SET_PENDING_OFFERS', payload: pendingOffers });
     } catch (error) {
       console.error('Error fetching offers:', error);
     } finally {
-      setIsLoading(false);
+      dispatch({ type: 'SET_LOADING', payload: false });
     }
   };
 
@@ -99,12 +118,7 @@ export default function OrdersPage() {
       fetchBuyOrders();
     }
   }, [session?.user.id]);
-  // useEffect(() => {
-  //   console.log(pendingOffersId);
-  // }, [pendingOffersId]);
-  // useEffect(() => {
-  //   console.log(orders);
-  // }, [orders]);
+
   const deleteOrder = async (orderId) => {
     const result = await Swal.fire({
       title: 'Usuń zamówienie',
@@ -137,13 +151,13 @@ export default function OrdersPage() {
       <div>
         <button
           className={`bg-mainBg px-2 py-1 ${view === 'buy' ? 'bg-red-300' : ''}`}
-          onClick={() => setView('buy')}
+          onClick={() => dispatch({ type: 'SET_VIEW', payload: 'buy' })}
         >
           kupuje
         </button>
         <button
           className={`bg-mainBg px-2 py-1 ${view === 'sell' ? 'bg-red-300' : ''}`}
-          onClick={() => setView('sell')}
+          onClick={() => dispatch({ type: 'SET_VIEW', payload: 'sell' })}
         >
           sprzedaje
         </button>
@@ -152,85 +166,84 @@ export default function OrdersPage() {
         <div className="bg-mainBg p-12 shadow-2xl mb-12">
           <h1>kupuje</h1>
           <PushNotification />
-          {buyOrders && (
-            <>
-              <div className="bg-brighterBg p-4 my-8 rounded-lg">
-                {orders.buyOrders.length > 0 ? (
-                  <>
-                    {orders?.buyOrders?.map((order) => (
-                      <>
-                        <div
-                          key={order._id}
-                          className="p-4   bg-mainBg flex justify-between"
-                        >
-                          <div>
-                            <p>Buyer: {order?.buyer?.name || 'N/A'}</p>
-                            <p>Seller: {order?.seller?.name || 'N/A'}</p>
-                            <p>Server: {order?.offer?.serverName || 'N/A'}</p>
-                            <p>Amount: {order?.currencyAmount || 0} yang</p>
-                            <p>Price: {order?.offer?.pricePLN || 0} PLN</p>
-                            <p
-                              className={
-                                order?.orderStatus === `accepted`
-                                  ? 'text-green-300'
-                                  : ''
-                              }
-                            >
-                              Status:{' '}
-                              {order?.orderStatus === 'pending'
-                                ? 'oczekuje'
-                                : order?.orderStatus || 'N/A'}
-                            </p>
-                          </div>
 
-                          <div>
-                            <button
-                              type="button"
-                              className="text-xl"
-                              onClick={() => deleteOrder(order._id)}
-                            >
-                              X
-                            </button>
-                          </div>
+          <>
+            <div className="bg-brighterBg p-4 my-8 rounded-lg">
+              {orders?.buyOrders.length > 0 ? (
+                <>
+                  {orders.buyOrders.map((order) => (
+                    <>
+                      <div
+                        key={order._id}
+                        className="p-4   bg-mainBg flex justify-between"
+                      >
+                        <div>
+                          <p>Buyer: {order?.buyer?.name || 'N/A'}</p>
+                          <p>Seller: {order?.seller?.name || 'N/A'}</p>
+                          <p>Server: {order?.offer?.serverName || 'N/A'}</p>
+                          <p>Amount: {order?.currencyAmount || 0} yang</p>
+                          <p>Price: {order?.offer?.pricePLN || 0} PLN</p>
+                          <p
+                            className={
+                              order?.orderStatus === `accepted`
+                                ? 'text-green-300'
+                                : ''
+                            }
+                          >
+                            Status:{' '}
+                            {order?.orderStatus === 'pending'
+                              ? 'oczekuje'
+                              : order?.orderStatus || 'N/A'}
+                          </p>
                         </div>
-                        {order?.orderStatus === 'accepted' && (
-                          <>
-                            <div className="flex justify-center mb-4 bg-mainBg p-4  shadow-md">
-                              <div className="flex flex-col items-center text-white">
-                                <p className="mb-4">
-                                  Czy transakcja przebiegła pomyślnie?
-                                </p>
-                                <div className="flex gap-6 justify-center">
-                                  <button className="bg-brighterBg px-6 py-2 text-lg rounded-lg text-red-300 hover:opacity-60">
-                                    Tak
-                                  </button>
-                                  <button className="bg-red-300 text-white text-lg px-6 py-2 rounded hover:opacity-60 transition">
-                                    Nie
-                                  </button>
-                                </div>
+
+                        <div>
+                          <button
+                            type="button"
+                            className="text-xl"
+                            onClick={() => deleteOrder(order._id)}
+                          >
+                            X
+                          </button>
+                        </div>
+                      </div>
+                      {order?.orderStatus === 'accepted' && (
+                        <>
+                          <div className="flex justify-center mb-4 bg-mainBg p-4  shadow-md">
+                            <div className="flex flex-col items-center text-white">
+                              <p className="mb-4">
+                                Czy transakcja przebiegła pomyślnie?
+                              </p>
+                              <div className="flex gap-6 justify-center">
+                                <button className="bg-brighterBg px-6 py-2 text-lg rounded-lg text-red-300 hover:opacity-60">
+                                  Tak
+                                </button>
+                                <button className="bg-red-300 text-white text-lg px-6 py-2 rounded hover:opacity-60 transition">
+                                  Nie
+                                </button>
                               </div>
                             </div>
-                          </>
-                        )}
-                      </>
-                    ))}
-                  </>
-                ) : (
-                  <>
-                    <p className="text-lg">
-                      Nie masz żadnych oczekujących zleceń kupna...
-                    </p>
-                    <p>
-                      Sprawdź oferty{' '}
-                      <span className="font-bold text-red-300">
-                        <Link href={'/marketplace/offers'}>tutaj</Link>
-                      </span>
-                    </p>
-                  </>
-                )}
-              </div>
-            </>
-          )}
+                          </div>
+                        </>
+                      )}
+                    </>
+                  ))}
+                </>
+              ) : (
+                <>
+                  <p className="text-lg">
+                    Nie masz żadnych oczekujących zleceń kupna...
+                  </p>
+                  <p>
+                    Sprawdź oferty{' '}
+                    <span className="font-bold text-red-300">
+                      <Link href={'/marketplace/offers'}>tutaj</Link>
+                    </span>
+                  </p>
+                </>
+              )}
+            </div>
+          </>
         </div>
       ) : (
         <div className="bg-mainBg p-12 shadow-2xl mb-12">
@@ -279,27 +292,39 @@ export default function OrdersPage() {
                         </div>
                         {order.orderStatus === 'pending' && (
                           // TODO zapytac czy aktualizowac zlecenie na minus currency co ktos chce kupc
-                          <div className=" flex gap-4 mb-8">
-                            <button
-                              onClick={() =>
-                                handleStatusChange('accepted', order._id)
-                              }
-                              className={`bg-mainBg px-4 py-2 rounded-lg hover:opacity-50 ${isLoading ? 'hover:opacity-100' : ''}`}
-                              disabled={isLoading}
-                            >
+                          <div className=" flex flex-col gap-4 mb-8">
+                            <div>
+                              <button
+                                onClick={() =>
+                                  handleStatusChange('accepted', order._id)
+                                }
+                                className={`bg-mainBg px-4 py-2 rounded-lg hover:opacity-50 ${isLoading ? 'hover:opacity-100' : ''}`}
+                                disabled={isLoading}
+                              >
+                                {' '}
+                                Akceptuj
+                              </button>
+                              <button
+                                onClick={() =>
+                                  handleStatusChange('rejected', order._id)
+                                }
+                                disabled={isLoading}
+                                className={`bg-red-100 text-mainBg px-4 py-2 rounded-lg hover:opacity-70 ${isLoading ? 'hover:opacity-100' : ''}`}
+                              >
+                                {' '}
+                                Odrzuć
+                              </button>
+                            </div>
+                            <div className="">
                               {' '}
-                              Akceptuj
-                            </button>
-                            <button
-                              onClick={() =>
-                                handleStatusChange('rejected', order._id)
-                              }
-                              disabled={isLoading}
-                              className={`bg-red-100 text-mainBg px-4 py-2 rounded-lg hover:opacity-70 ${isLoading ? 'hover:opacity-100' : ''}`}
-                            >
-                              {' '}
-                              Odrzuć
-                            </button>
+                              <button
+                                disabled={isLoading}
+                                className={`bg-red-100 text-mainBg px-4 py-2 rounded-lg hover:opacity-70 ${isLoading ? 'hover:opacity-100' : ''}`}
+                              >
+                                {' '}
+                                Zaaktualizuj zlecenie o sprzedaną kwotę
+                              </button>
+                            </div>
                           </div>
                         )}
                       </>
