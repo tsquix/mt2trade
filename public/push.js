3879786ab2e09dab88f2dde7374fc696eb397720
tsquix <tsquix@gmail.com>
2025-09-11 13:23:21 +0200

    vapid to env

--- public/push.js
+++ public/push.js
@@ -1,9 +1,8 @@
 var push = require('web-push');
 
 let vapidKeys = {
-  publicKey:
-    'BEy9M0NM8txQ1yRYTHXFYL5588f3pR1u-em5StnrvXLaLLW-EKzyWKjinlEe7CGij5WfJ75dyC9rKXB8NDBDCRI',
-  privateKey: 'YDg0QmSK4XKWm8DGKHWIBdh154cTyvKN7O6nzVJFo14',
+  publicKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
+  privateKey: process.env.VAPID_PRIVATE_KEY,
 };
 
 push.setVapidDetails(
